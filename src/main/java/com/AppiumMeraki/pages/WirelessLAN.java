ce79846a4174122e382425012a52426048f57e1d
dhumeshruti <dhumeshruti@gmail.com>
2022-04-30 13:42:20 -0400

    Update

--- src/main/java/com/AppiumMeraki/pages/WirelessLAN.java
+++ src/main/java/com/AppiumMeraki/pages/WirelessLAN.java
@@ -3,8 +3,6 @@ package com.AppiumMeraki.pages;
 import java.util.ArrayList;
 
 import java.util.List;
-//import java.util.concurrent.TimeUnit;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
